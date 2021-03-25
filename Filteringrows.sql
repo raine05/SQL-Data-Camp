d1a637c00a1198ae86b52dd84284fb908fb74221
loanne5 <58616743+loanne5@users.noreply.github.com>
2021-03-25 23:26:48 +0800

    Create Filteringrows.sql

---
+++ Filteringrows.sql
@@ -0,0 +1,13 @@
+--Filtering results
+/*
+= equal
+<> not equal
+< less than
+> greater than
+<= less than or equal to
+>= greater than or equal to
+*/
+
+--WHERE 
+--Get all details for all films released in 2016.
+SELECT * FROM films where release_year = 2016

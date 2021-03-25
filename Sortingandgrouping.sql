81befa9d49b09681b834e5100b0c8d1d8fe8ff4a
loanne5 <58616743+loanne5@users.noreply.github.com>
2021-03-26 00:26:07 +0800

    Create Sortingandgrouping.sql

---
+++ Sortingandgrouping.sql
@@ -0,0 +1,5 @@
+--ORDER BY keyword is used to sort results in ascending or descending order according to the values of one or more columns.
+--DESC newest to oldest
+SELECT title
+FROM films
+ORDER BY release_year DESC;
